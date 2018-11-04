8893ff2a4dd6636b66fe86eb74871b212c767093
Noricssunny <43686044+Noricssunny@users.noreply.github.com>
2018-11-04 15:55:00 -0800

    Updated project with fbx and spine animations.

---
+++ UnityProject/Assets/Scripts/GhostController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class GhostController : MonoBehaviour {
+
+	public SkeletonAnimation skeletonAnimation;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown("space"))
+		{
+			skeletonAnimation.loop = true;
+			skeletonAnimation.AnimationName = "scare";
+
+		}
+		if (Input.GetKeyDown ("d")) 
+		{
+			skeletonAnimation.loop = true;
+			skeletonAnimation.AnimationName = "run";
+
+			this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+		}
+		if (Input.GetKeyDown ("s")) 
+		{
+			skeletonAnimation.loop = true;
+			skeletonAnimation.AnimationName = "idle";
+
+		}
+		if (Input.GetKeyDown ("a")) 
+		{
+			skeletonAnimation.loop = true;
+			skeletonAnimation.AnimationName = "run"; 
+
+			this.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+		}
+		if (Input.GetKeyDown ("w")) 
+		{
+			skeletonAnimation.loop = false;
+			skeletonAnimation.AnimationName = "death";
+
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/HuManController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuManController : MonoBehaviour 
+{
+	public Animator animator;
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown ("d")) 
+		{
+			animator.SetTrigger ("moving");
+			this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+		}
+		if (Input.GetKeyDown("s"))
+		{
+			animator.SetTrigger ("idling");
+		}
+		if (Input.GetKeyDown ("space")) 
+		{
+			animator.SetTrigger ("jumping");
+		}
+		if (Input.GetKeyDown ("w")) 
+		{
+			animator.SetTrigger ("dying");
+		}
+		if (Input.GetKeyDown ("a")) 
+		{
+			animator.SetTrigger ("moving");
+			this.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+		}
+		if (Input.GetKeyDown("e"))
+		{
+			animator.SetTrigger ("clapping");
+		}
+	}
+}
